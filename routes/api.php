525ace525c268196a9104b8a436ee6068fb2708d
cartbim <cartbim@gmail.com>
2021-06-11 11:01:34 +0100

    Bolier plate created

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/search', function ($id) {
+    return product;
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
