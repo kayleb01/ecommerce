00280ff66087906a57ad3fa18fe564a4cba96104
adewumi0550 <adewumiadewale493@gmail.com>
2021-06-13 09:52:34 +0100

    Categories  added

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
-
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/search', function ($id) {
-    return product;
-});
+// Route::get('/search', function ($id) {
+//     return product;
+// });
+
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::post('/add-category', [CategoryController::class, 'store']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
