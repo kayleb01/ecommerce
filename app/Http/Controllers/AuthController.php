87cc44dffe285b1a1a61be88e9de92415c3611c3
Hussaini Muhammad Auwal <hauwal4969@gmail.com>
2021-06-13 17:03:56 +0100

    registration/login-apis created

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller
      */
     public function login(Request $request){
         $validator = Validator::make($request->all(), [
-            'username' => 'required|email',
+            'username' => 'required|string',
             'password' => 'required|string|min:6',
         ]);
 
