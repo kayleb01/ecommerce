6fc4db9a7d034e8c6a4da16606e82dfd0983be26
Hussaini Muhammad Auwal <hauwal4969@gmail.com>
2021-06-23 06:43:44 +0100

    error

---
+++ app/Http/Resources/ShopResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ShopResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        //return parent::toArray($request);
+
+        return [
+            'name' => $this->name,
+            'address' => $this->address,
+            'description' => $this->description,
+            'product' => $this->product,
+            ]
+    }
+}
